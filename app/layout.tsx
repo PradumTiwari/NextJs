90092ff7c946e9fa24b87ef615e41a3562555a51
Pradum <satendarpradum@gmail.com>
2024-10-14 01:15:00 +0530

    Added Routes Logic

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="p-2 text-xl">Medium</div>
         {children}
       </body>
     </html>
---
+++ app/signin/page.tsx
@@ -0,0 +1,6 @@
+
+const signin=()=>{
+    return <div className="p-2 text-xl text-red-600">SignIn</div>
+}
+
+export default signin;
